aee6225d01f0955ce7b2bc1cd335efe558fa96df
xQrewo <78736102+urewo396@users.noreply.github.com>
2024-08-16 09:21:43 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+function App() {
+
+  useEffect(() => {
+    const updateCursor = (e) => {
+      document.body.style.setProperty('--x', `${e.clientX}px`);
+      document.body.style.setProperty('--y', `${e.clientY}px`);
+    };
+
+    window.addEventListener('mousemove', updateCursor);
+
+    return () => {
+      window.removeEventListener('mousemove', updateCursor);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <div className="navbar-title">Portfolio</div>
+        <div className="navbar-links">
+          <a href="#home" className="navbar-link">Home</a>
+          <a href="#projects" className="navbar-link">Projects</a>
+          <a href="#about" className="navbar-link">About</a>
+          <a href="#contact" className="navbar-link">Contact</a>
+        </div>
+      </nav>
+      <main>
+        <h1>Hey, I'm Max and this is my Portfolio</h1>
+        <p>I'm a student that want to become a software developer some day and this site is to showcase my projects.</p>
+        <a href="#projects" className="btn">View My Projects</a>
+      </main>
+    </div>
+  );
+}
+
+export default App;
